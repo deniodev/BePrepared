b287221f3da74c8e8ef571119b9d96a1e8c60727
Denio Nhanale <dnhanale@gmail.com>
2024-07-17 11:43:53 +0200

    Add Auth Routes

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -1,14 +1,26 @@
 import { FastifyInstance } from "fastify";
 import { SubscriberController } from "../controllers/SubscriberController";
+import { AuthController } from "../controllers/authController";
 
 const subscriberController = new SubscriberController();
+const authController = new AuthController();
 
 export async function routes(fastify: FastifyInstance) {
-  fastify.get("/", async function handler(request, reply) {
+  fastify.get("/alerts", async function handler(request, reply) {
     return { hello: "dasn" };
   });
 
   fastify.post("/subscribers", (request, reply) =>
     subscriberController.create(request, reply)
+  );
+  fastify.put("/subscribers", (request, reply) =>
+    subscriberController.update(request, reply)
+  );
+
+  fastify.post("/auth/subscribers/otp", (request, reply) =>
+    authController.authOtp(request, reply)
+  );
+  fastify.post("/auth/subscribers", (request, reply) =>
+    authController.loginSubscriber(request, reply)
   );
 }
