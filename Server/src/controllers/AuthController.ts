e93e938b6da4b077b499b63b911ac2f5f1c9546e
Denio Nhanale <dnhanale@gmail.com>
2024-07-17 11:44:05 +0200

    Add Auth Controller

---
+++ Server/src/controllers/AuthController.ts
@@ -0,0 +1,59 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { redis } from "../database/redis";
+import { db } from "../database";
+import { generateSixDigitsNumber } from "../utils/utils";
+
+export class AuthController {
+  async authOtp(request: FastifyRequest, reply: FastifyReply) {
+    const authSchema = z.object({
+      otp: z.number(),
+      deviceId: z.string(),
+    });
+    const { otp, deviceId } = authSchema.parse(request.body);
+
+    const phone = await redis.get(`otp_${otp}`);
+    console.log(phone);
+
+    if (!phone) {
+      return reply.status(401).send({ error: "OTP Invalido!" });
+    }
+
+    const subscriber = await db.subscriber.update({
+      data: {
+        deviceId,
+        verified: true,
+      },
+      where: {
+        phone,
+      },
+      include: {
+        province: true,
+        district: true,
+      },
+    });
+
+    await redis.delete(`otp_${otp}`);
+    return reply.send(subscriber);
+  }
+
+  async loginSubscriber(request: FastifyRequest, reply: FastifyReply) {
+    const subscriberSchema = z.object({
+      phone: z.string().regex(/^8[2-7]\d{7}/),
+    });
+    const { phone } = subscriberSchema.parse(request.body);
+    const subscriber = await db.subscriber.findUnique({
+      where: {
+        phone,
+      },
+    });
+
+    if (!subscriber)
+      return reply.status(400).send({ error: "Usuario nao encontrado" });
+
+    const otp = generateSixDigitsNumber();
+    console.log(otp);
+    await redis.set(`otp_${otp}`, phone, 60 * 3);
+    return reply.status(204).send();
+  }
+}
