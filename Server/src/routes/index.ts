dd5f78c73e7fcf4d856d3a5ed0f4e54a9f181b18
Denio Nhanale <dnhanale@gmail.com>
2024-07-22 11:33:09 +0200

    Add Alert Route

--- Server/src/routes/index.ts
+++ Server/src/routes/index.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from "fastify";
 import { SubscriberController } from "../controllers/SubscriberController";
 import { AuthController } from "../controllers/AuthController";
 import { NotificationController } from "../controllers/NotificationController";
+import { AlertController } from "../controllers/AlertController";
+import { authHook } from "../hooks/auth";
 
 const subscriberController = new SubscriberController();
 const authController = new AuthController();
 const notificationController = new NotificationController();
+const alertController = new AlertController();
 
 export async function routes(fastify: FastifyInstance) {
   fastify.get("/alerts", async function handler(request, reply) {
@@ -34,5 +37,9 @@ export async function routes(fastify: FastifyInstance) {
   );
   fastify.get("/notifications/:phone", (request, reply) =>
     notificationController.show(request, reply)
+  );
+
+  fastify.post("/alerts", { preHandler: authHook }, (request, reply) =>
+    alertController.create(request, reply)
   );
 }
