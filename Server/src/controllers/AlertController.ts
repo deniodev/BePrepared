6f86a597ffbc41d7e43e5469e46ba0c6768768d0
Denio Nhanale <dnhanale@gmail.com>
2024-07-26 17:35:49 +0200

    Update Alert Controller

--- Server/src/controllers/AlertController.ts
+++ Server/src/controllers/AlertController.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { getMessaging } from "firebase-admin/messaging";
 import { z } from "zod";
 import { db } from "../database";
 
@@ -10,28 +11,106 @@ export class AlertController {
       provinceId: z.string(),
       districtId: z.string(),
     });
-    const { title, message, districtId, provinceId } = alertSchema.parse(
-      request.body
-    );
-    const district = await db.district.findUnique({
-      where: {
-        id: districtId,
-        provinceId,
-      },
-    });
-    if (!district) {
-      return reply
-        .status(400)
-        .send({ error: "Distrito nao pertence a provincia!" });
+
+    try {
+      // Validate request body
+      const { title, message, districtId, provinceId } = alertSchema.parse(
+        request.body
+      );
+
+      // Check if the district belongs to the province
+      const district = await db.district.findUnique({
+        where: {
+          id: districtId,
+          provinceId,
+        },
+      });
+
+      if (!district) {
+        return reply
+          .status(400)
+          .send({ error: "District does not belong to the province!" });
+      }
+
+      // Get subscribers' device IDs
+      const subscribersDeviceIds = await db.subscriber.findMany({
+        select: {
+          deviceId: true,
+        },
+        where: {
+          districtId,
+          deviceId: {
+            not: null,
+          },
+          verified: true,
+        },
+      });
+
+      // Create alert in the database
+      const alert = await db.alert.create({
+        data: {
+          title,
+          message,
+          districtId,
+          provinceId,
+        },
+        select: {
+          id: true,
+          title: true,
+          message: true,
+          districtId: true,
+          provinceId: true,
+        },
+      });
+
+      const tokens = subscribersDeviceIds.map((deviceId) =>
+        String(deviceId.deviceId)
+      );
+
+      if (tokens.length === 0) {
+        console.log("No verified subscribers with device IDs found.");
+        return reply.send(alert);
+      }
+
+      const alertNotification = {
+        data: alert,
+        tokens,
+      };
+
+      // Send notifications
+      const response = await getMessaging().sendMulticast(alertNotification);
+      console.log(`${response.successCount} messages were sent successfully`);
+
+      return reply.send(alert);
+    } catch (error) {
+      console.error("Error creating alert:", error);
+
+      if (error instanceof z.ZodError) {
+        return reply
+          .status(400)
+          .send({ error: "Invalid input data", details: error.errors });
+      }
+
+      return reply.status(500).send({ error: "Internal Server Error" });
     }
-    const alert = await db.alert.create({
-      data: {
-        title,
-        message,
-        districtId,
+  }
+
+  async list(request: FastifyRequest, reply: FastifyReply) {
+    const querySchema = z.object({
+      provinceId: z.string().optional(),
+      districtId: z.string().optional(),
+    });
+    const { provinceId, districtId } = querySchema.parse(request.query);
+    const alerts = await db.alert.findMany({
+      where: {
         provinceId,
+        districtId,
+      },
+      include: {
+        province: true,
+        district: true,
       },
     });
-    return reply.send(alert);
+    return reply.send(alerts);
   }
 }
